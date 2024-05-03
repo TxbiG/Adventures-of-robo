5d7b7008b00cb04f72df99bc397d9a4e52442c64
Toby <126118957+TxbiG@users.noreply.github.com>
2024-05-03 20:51:08 +0100

    Update splashscreen.cpp

--- Game/Game/splashscreen.cpp
+++ Game/Game/splashscreen.cpp
@@ -18,16 +18,8 @@ splashscreen::splashscreen(SDL_Renderer* ren)
 	this->text = TTF_OpenFont("assets/font.ttf", 18);
 
 	UI* splash1 = new UI("Adventures of robo", 640, 480, title);
-	UI* splash2 = new UI("Controls", 610, 610, subtitle);
-	UI* splash3 = new UI("Left: A", 570, 670, text);
-	UI* splash4 = new UI("Right: D", 560, 730, text);
-	UI* splash5 = new UI("Jump: Space", 590, 790, text);
 
 	splash1->render(ren);
-	splash2->render(ren);
-	splash3->render(ren);
-	splash4->render(ren);
-	splash5->render(ren);
 
 	SDL_RenderPresent(ren);
 }
